381929211214367fca3f1adf781f7726a771bea9
NourALDS <noureddinealsati@gmail.com>
2026-06-13 10:36:41 +0300

    Print template and css node details

--- src/AST/css/CssNode.java
+++ src/AST/css/CssNode.java
@@ -6,4 +6,9 @@ public abstract class CssNode extends AstNode {
     protected CssNode(String nodeName, int line) {
         super(nodeName, line);
     }
+
+    @Override
+    public String describe() {
+        return details();
+    }
 }
--- src/AST/template/TemplateNode.java
+++ src/AST/template/TemplateNode.java
@@ -6,4 +6,9 @@ public abstract class TemplateNode extends AstNode {
     protected TemplateNode(String nodeName, int line) {
         super(nodeName, line);
     }
+
+    @Override
+    public String describe() {
+        return details();
+    }
 }
